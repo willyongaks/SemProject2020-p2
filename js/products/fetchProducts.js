c5d0783befc3b27249345fc64b3920682b5cedb3
willyongaks <83573335+willyongaks@users.noreply.github.com>
2022-09-20 12:10:37 +0200

    Update fetchProducts.js

--- js/products/fetchProducts.js
+++ js/products/fetchProducts.js
@@ -1,5 +1,4 @@
 import { baseUrl } from "../settings/baseUrl.js";
-// import { displayMessage } from "../components/displayMessage.js";
 import { toggleProducts } from "./toggleProducts.js";
 
 const itemsUrl = baseUrl;
